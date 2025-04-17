b8badf146dbcf29a3f8f39fb16abaffe1563ed7e
Mohamed Desoky <mae.desoky@gmail.com>
2025-04-17 01:26:12 -0400

    Included all routes; still testing postman

---
+++ block34/part1/server/db.js
@@ -0,0 +1,10 @@
+// server/db.js
+const pkg = require('pg');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const client = new pkg.Client({
+  connectionString: process.env.DATABASE_URL || 'postgres://mdesoky:Welcome2024@localhost:5432/acme_reservations_db'
+});
+
+module.exports = client;
